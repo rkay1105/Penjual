42a82e25336eda6e74d77dcb766f1d0704a04b9f
Rendi <rendhiyakasyfill@gmail.com>
2019-08-28 15:38:29 +0700

    Initial commit

---
+++ app/src/main/java/com/rendi/penjual/Formulir.java
@@ -0,0 +1,14 @@
+package com.rendi.penjual;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Formulir extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulir);
+    }
+}
---
+++ app/src/main/java/com/rendi/penjual/Login.java
@@ -0,0 +1,18 @@
+package com.rendi.penjual;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
